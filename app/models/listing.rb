62fa3914c5b263ac651c6f065add48510b651ba4
gmasouras <masourasgeorge@gmail.com>
2017-01-23 22:06:01 +0200

    Install and set up Devise gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,4 @@
 class Listing < ApplicationRecord
   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "noimage.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ 
 end
